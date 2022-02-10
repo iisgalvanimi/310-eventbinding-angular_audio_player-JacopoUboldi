e190025d0b573f2879619c5c057922ba320a5a21
JacopoUboldi <uboldi.jacopo@iisgalvanimi.edu.it>
2022-02-10 08:45:40 +0000

    Event Binding - Angular_Audio_Player

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import {Component,OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  audio : HTMLAudioElement ;
+  duration: number = 0; 
+  title = '310-eventbinding-angular_audio_player-JacopoUboldi';
+
+  playAudio(){
+    this.duration = this.audio.duration
+    this.audio.load();
+    this.audio.play();
+  }
+
+  stopAudio(){
+    this.audio.pause()
+  }    
+  
+  constructor(){}
+
+  ngOnInit() {
+    this.audio = new Audio();
+    this.audio.src = "../asset/bensound-endlessmotion.mp3"   
+  }
+
+}
+  
